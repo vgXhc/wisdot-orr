8d475924406d0cf6be736276c8cc2fff69a7f27e
Harald Kliems <kliems@gmail.com>
2022-08-23 18:17:12 -0500

    code all works now

--- generate_markdown.R
+++ generate_markdown.R
@@ -1,5 +1,6 @@
 library(msgxtractr)
 library(tidyverse)
+library(here)
 
 
 
@@ -7,57 +8,37 @@ library(tidyverse)
 files <- list.files("data/All Files/")
 
 # extract and save attachments and return paths
-write_attachments <- function(msg_file){
+write_msg_files <- function(msg_file){
   msg <- read_msg(paste0("data/All Files/", msg_file))
   dir_name <- paste0("output/", msg_file)
   dir.create(dir_name)
-  save_attachments(msg_obj = dat, path = dir_name)
+  save_attachments(msg_obj = msg, path = dir_name)
   #return path and file names for attachments
   attachment_names <- list.files(dir_name)
-  as.data.frame(attachment_names) %>% 
-    mutate(email = msg_file,
-           from = unlist(msg$headers$From),
-           body = paste(msg$body$text, collapse = "\n"))
+  attachment_paths <- paste0(msg_file, "/", attachment_names)
+  
+  # paths <- as.data.frame(attachment_names) %>% 
+  #   mutate(email = msg_file,
+  #          from = unlist(msg$headers$From),
+  #          body = paste(msg$body$text, collapse = "\n"))
+  
+  paths <- data_frame(email = msg_file,
+                   from = unlist(msg$headers$From),
+                   to = unlist(msg$headers$To),
+                   cc = unlist(msg$headers$CC),
+                   subject = unlist(msg$headers$Subject),
+                   date = unlist(msg$headers$Date),
+                   body = paste(msg$body$text, collapse = "\n"),
+                   attachments = list(attachment_paths))
+  rmarkdown::render(
+    input = "output_template.Rmd",
+    #output_file = paste0(paths[1,]$paths, ".html"),
+    output_file = here("output", paste0(msg_file, ".html"))
+  )
 }
 
 
-paths <- map_dfr(files[2], write_attachments)
+# paths <- map_dfr(files[2], write_attachments)
 
-write_attachments(files[1])
-
-dat <- read_msg(paste0("data/All Files/", files[2]))
-
-msg_text <- dat$body$text
-
-msgxtractr::
-
-
-dat
-unlist(dat)
-
-#get vector of all attachment names
-attachment_names <- map_chr(dat$attachments, "long_filename")
-
-#create text output
-msg <- paste0("From: ", dat$sender$sender_name, " (", dat$sender$sender_email,")\n",
-       "To: ", dat$headers$To, "\n",
-       "Subject", dat$headers$Subject, "\n",
-       "Attachments: ", attachment_names, "\n",
-       dat$body$html)
-
-
-# code for rendering parametrized report
-for(i in 1:length(hospitals)){
-  rmarkdown::render(
-    input = "surveillance_report.Rmd",
-    output_file = str_glue("output/Report_{hospitals[i]}_{Sys.Date()}.docx"),
-    params = list(hospital  = hospitals[i]))
-}       
-
-rmarkdown::render(
-  input = "output_template.Rmd",
-  #output_file = paste0(paths[1,]$paths, ".html"),
-  output_file = "output/test.docx"
-  )
-
+walk(files, write_attachments)
 
